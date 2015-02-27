2f88835c8a426e77996f2d96e17b2bb509be73e1
Noah Finnerman <nonothetoad@gmail.com>
2015-02-27 14:17:22 -0500

    Made a good amount of changes on the design and layout, but still also are able to remove items by clicking on them.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,13 @@ $('.big-button').click(function(){
 var  toAdd =$('input[name=new-shoe]').val();
 $('#msg').append('<li class="item">' + toAdd +  '</li>');
 
-
-
-
 })
 
+$('ul').on('click', '.item', function(event){
+  $(this).remove();
+
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});
 });
 
